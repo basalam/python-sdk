93a9098c28e7dfd36f558771cecf962057f62e92
fatemefallah <fatemefallah99@gmail.com>
2025-10-18 17:10:58 +0330

    feat: update version

--- src/basalam_sdk/version.py
+++ src/basalam_sdk/version.py
@@ -4,7 +4,7 @@ Version information for the Basalam SDK.
 This module centralizes version management and User-Agent generation.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __sdk_name__ = "basalam-python-sdk"
 
 
